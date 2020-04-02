b76d050914d952c52a794b86cb5fd8373539df34
Kyle Taylor <kyletaylored@gmail.com>
2020-04-01 21:21:33 -0500

    Use WP password reset

--- private/scripts/reset.php
+++ private/scripts/reset.php
@@ -7,7 +7,7 @@ $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
 
 // Import database
 $cmd = "wp db import ${path}/database.sql";
-echo(passthru($cmd));
+passthru($cmd);
 
 // Get environment variables, create password.
 $email = $_POST['user_email'];
@@ -17,37 +17,10 @@ $password = bin2hex(random_bytes(10));
 $cmd = "wp user update 1 --user_email=${email} --user_pass=${password}";
 passthru($cmd);
 
-// Prepare
-$site_id = $_POST['site_id'];
-$subject = "Pantheon Crisis Response Site | Password Reset | [${site_id}]";
-
-$message = "
-<html>
-<head>
-    <title>Pantheon Crisis Response - Password Reset</title>
-</head>
-<body>
-    <p>Your temporary password has been reset:</p>
-    <ul>
-        <li><strong>Username:</strong> superuser</li>
-        <li><strong>Password:</strong> ${password}</li>
-    </ul>
-</body>
-</html>
-";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <no-reply@pantheon.io>' . "\r\n";
-// $headers .= 'Cc: support@pantheon.io' . "\r\n";
-
-// Send email
-mail($email,$subject,$message,$headers);
+// Reset admin password
+passthru('wp user reset-password 1');
 
 // Clear cache, because why not.
-passthru( 'wp cache flush' );
+passthru('wp cache flush');
 
 print( "\n==== WP Reset Complete ====\n" );
