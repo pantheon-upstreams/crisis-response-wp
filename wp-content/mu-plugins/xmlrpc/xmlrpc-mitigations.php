1ae22c0429114b06970b831beacdbdaf6522f583
Kyle Taylor <kyletaylored@gmail.com>
2020-05-01 00:31:35 -0500

    Testing XMLRPC mitigations

---
+++ wp-content/mu-plugins/xmlrpc/xmlrpc-mitigations.php
@@ -0,0 +1,7 @@
+<?php
+
+// Testing XMLRPC mitigations.
+remove_action( 'wp_head', 'rsd_link');
+remove_action( 'wp_head', 'wlwmanifest_link');
+remove_action( 'wp_head', 'wp_shortlink_wp_head');
+remove_action( 'wp_head', 'wp_generator');
